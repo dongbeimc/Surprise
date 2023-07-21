47dac27642d1c73a69fc7b4c0c714ff365db6909
dongbeimc <ClarkLerty@outlook.com>
2023-07-21 18:42:03 +0800

    Change message

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,7 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git!");
         }
     }
 }
